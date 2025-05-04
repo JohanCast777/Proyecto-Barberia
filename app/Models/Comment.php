a7b5652cc4589b02ee7d92dcb2d38eb378470aa6
Johan Castano <castanojohan01@gmail.com>
2025-05-04 04:55:04 -0500

    Pimer Commit

---
+++ app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    //
+    use HasFactory;
+    protected $fillable = ['*'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'foreign_key', 'primary_key'); 
+    }
+
+}
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $user = new User();
+        $user->name = 'John Doe';
+        $user->email = 'johndoe@example.com';
+        $user->password = Hash::make('password');
+        $user->save(); 
+
+    }
+}
