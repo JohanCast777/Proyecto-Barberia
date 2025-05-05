451b448dc8b3e04e2cdecf4ddc595f9e78eafa7c
Johan Castano <castanojohan01@gmail.com>
2025-05-05 15:25:44 -0500

    Quinto Commit-Botones

--- resources/views/livewire/counter-component.blade.php
+++ resources/views/livewire/counter-component.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <p>El contador es: {{ $count }}</p>
+    <button style="background-color: #3b82f6; color: white; font-weight: bold; padding: 10px 20px; border-radius: 5px; border: none; cursor: pointer;" 
+            onmouseover="this.style.backgroundColor='#2563eb'" 
+            onmouseout="this.style.backgroundColor='#3b82f6'" 
+            wire:click="increment">
+        Incrementar
+    </button>
+    <button style="background-color: #ef4444; color: white; font-weight: bold; padding: 10px 20px; border-radius: 5px; border: none; cursor: pointer;" 
+            onmouseover="this.style.backgroundColor='#dc2626'" 
+            onmouseout="this.style.backgroundColor='#ef4444'" 
+            wire:click="decrement">
+        Disminuir
+    </button>
 </div>
